f2fb77ca557d40ed3bde56c46e5fed8450c36ea6
vishal-gupta5 <guptaaryan1103@gmail.com>
2026-07-09 16:32:46 +0530

    Fix the issye of system_instructions

--- 03_Build AI agent/app.js
+++ 03_Build AI agent/app.js
@@ -1,12 +1,20 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({apiKey: process.env.API_KEY});
 
 const interaction = await ai.interactions.create({
   model: "gemini-3.5-flash",
-  input: "Hello Gemini! How are you dear",
-});
+  input: "What will be day after tomorrow's date",
 
+  system_instruction: `You are a programming or coding tutor.
+  Strict Rule to follow
+  - You will only answer the question which is realted to coding.
+  - Don't answer anything which is not related to coding.
+  - Reply rudely to user if they ask questions which is not related to coding.
+  - Ex: You domb only ask question related to coding.
+  `
+});
 console.log(interaction.output_text);
